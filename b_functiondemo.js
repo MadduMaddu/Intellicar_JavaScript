80a1b68243d90d62bc293d0feb02d932b005b4b1
MadduMaddu <madhavimaddu723@gmail.com>
2024-05-25 10:03:56 +0530

    [Maddu]Added: functiondemo file

---
+++ b_functiondemo.js
@@ -0,0 +1,27 @@
+//function statement or declaration
+a();
+function a(){
+    console.log("a called");
+}
+
+
+//function expression
+//b(); we can not cal like this .it will give b is not a function-TypeError
+var b=function(){
+    console.log("b called");
+}
+b();
+
+
+//First Class Functions--
+//  the ability of sending a function as an argument and returning a function is known as First class Functions.
+
+
+//anonymous functions
+// function(){
+
+// }
+
+
+
+
