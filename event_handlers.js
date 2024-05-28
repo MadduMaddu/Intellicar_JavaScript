03beae1f5439b16493e53964dc7f35a07c18d05e
MadduMaddu <madhavimaddu723@gmail.com>
2024-05-28 10:09:56 +0530

    [Maddu]Added: event handling file

---
+++ event_handlers.js
@@ -0,0 +1,16 @@
+function onClick(){
+    document.getElementById('res').innerText=`box-1 clicked`;
+}
+function mouseOver(){
+    document.getElementById('res').innerText=`mouse is on box-2`;
+}
+function keyUp(){
+    console.log("key up");
+}
+function keyDown(){
+    console.log("key Down");
+
+}
+function keyPress(){
+    console.log("key Press");
+}
