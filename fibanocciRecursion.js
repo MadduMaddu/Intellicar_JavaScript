66877ec6d8d4142ca05b380ef53b1ff3c5273ec6
MadduMaddu <madhavimaddu723@gmail.com>
2024-06-01 12:38:13 +0530

    [Maddu]Added:recursive fibanocci file

--- fibanocciRecursion.js
+++ fibanocciRecursion.js
@@ -10,3 +10,9 @@ console.log(fibanocci(0));
 console.log(fibanocci(1));
 console.log(fibanocci(6));
 console.log(fibanocci(8));
+//here time complexity is , for example take n as 4
+//                          f4              2^0
+//                      f3      f2          2^1
+//                  f2    f1  f1   f0       2^2
+//                                          |||
+// time complexity here is O(2^n)
